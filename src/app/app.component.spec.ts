c1777dff2aa8a2170c78116056c564172cc48957
Viktor Ens <v.ens86@yahoo.de>
2024-11-13 16:35:37 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'kalle-shop' title`, () => {
+  it(`should have the "Kalle's Shop" title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('kalle-shop');
+    expect(app.title).toEqual("Kalle's Shop");
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, kalle-shop');
+    expect(compiled.querySelector('h1')?.textContent).toContain("Kalle's Shop");
   });
 });
