a4e5e577da2781f59a01fff89ede889dae1c858d
Viktor Ens <v.ens86@yahoo.de>
2024-11-16 12:08:23 +0100

    add footer component template

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -4,6 +4,7 @@ import { ProductListComponent } from '../product-list/product-list.component';
 import { BenefitsComponent } from '../../components/benefits/benefits.component';
 import { CatergoriesHomeComponent } from '../../components/catergories-home/catergories-home.component';
 import { PopularProductsComponent } from '../../components/popular-products/popular-products.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-main',
@@ -14,6 +15,7 @@ import { PopularProductsComponent } from '../../components/popular-products/popu
     BenefitsComponent,
     CatergoriesHomeComponent,
     PopularProductsComponent,
+    FooterComponent,
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
