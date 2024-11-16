c47c48051e5cce0f73cb3c7b941b10b8003056fe
Viktor Ens <v.ens86@yahoo.de>
2024-11-16 08:25:11 +0100

    add benefits component

--- src/app/pages/main/main.component.spec.ts
+++ src/app/pages/main/main.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainComponent } from './main.component';
+import { ProductsService } from '../../services/products.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('MainComponent', () => {
   let component: MainComponent;
@@ -8,9 +11,13 @@ describe('MainComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainComponent]
-    })
-    .compileComponents();
+      imports: [MainComponent],
+      providers: [
+        ProductsService,
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MainComponent);
     component = fixture.componentInstance;
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { ProductListComponent } from '../product-list/product-list.component';
+import { BenefitsComponent } from '../../components/benefits/benefits.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeroComponent, ProductListComponent],
+  imports: [HeroComponent, ProductListComponent, BenefitsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
